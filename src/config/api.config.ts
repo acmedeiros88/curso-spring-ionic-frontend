fc84d05dd58375c034705c5099b262cea670cd3a
Adriano Medeiros <adriano_cardoso@hotmail.com.br>
2021-10-29 05:46:42 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://curso-spring-adriano.herokuapp.com",
     bucketBaseUrl: "https://curso-spring-ionic-du.s3.sa-east-1.amazonaws.com"
 }
